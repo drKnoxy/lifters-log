82c722f3c4382828380daf9582cd8b578abf57af
Adam Willoughby-Knox <adam.w.knox@gmail.com>
2022-08-04 01:53:04 +0000

    fix: source sw properly

--- js/app.js
+++ js/app.js
@@ -4,6 +4,32 @@
 angular.module('llApp', ['ngStorage']);
 
 })();
+
+const registerServiceWorker = async () => {
+    if ('serviceWorker' in navigator) {
+        try {
+            const registration = await navigator.serviceWorker.register(
+                '/sw.js',
+                {
+                    scope: '/',
+                }
+            );
+            if (registration.installing) {
+                console.log('Service worker installing');
+            } else if (registration.waiting) {
+                console.log('Service worker installed');
+            } else if (registration.active) {
+                console.log('Service worker active');
+            }
+        } catch (error) {
+            console.error(`Registration failed with ${error}`);
+        }
+    }
+};
+
+
+registerServiceWorker();
+
  
 // /Users/awknox/code/lifters-log/_js/app.controller.js
 (function(){
